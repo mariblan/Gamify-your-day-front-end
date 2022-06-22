8c1ae89d31f28530b644c08dbf384264b4db04e2
Lilian Forger <lilianforger@gmail.com>
2022-06-22 09:46:04 +0200

    Context trialing, further DB querying

--- src/App.js
+++ src/App.js
@@ -7,31 +7,24 @@ import TaskTimer from "./components/gameProgress/timer/taskTimer";
 import MyTaskList from "./components/gameProgress/gameSetup/myTaskList";
 import TaskSuccess from "./components/gameProgress/timer/taskSuccess";
 import TaskFailure from "./components/gameProgress/timer/taskFailure";
-import { TaskContext } from "./components/gameProgress/timer/taskContext";
-import { useState } from "react";
+import { TaskProvider } from "./components/gameProgress/timer/taskContext";
+//import { useState, useContext } from "react";
 
 function App() {
-  const [gottenTask, setGottenTask] = useState({
-    category: "",
-    taskdescription: "",
-    taskid: 0,
-    taskName: "",
-    taskTime: {},
-  });
   return (
-    <TaskContext.Provider value={{ gottenTask, setGottenTask }}>
-      <Router>
-        <Routes>
-          <Route path="alltasks" element={<AllTasks />} />
-          <Route path="mytasks" element={<MyTaskList />} />
-          <Route path="petselection" element={<AnimalSelection />} />
-          <Route path="gamego" element={<GetTask />} />
-          <Route path="tasktimer" element={<TaskTimer />} />
-          <Route path="tasksuccess" element={<TaskSuccess />} />
-          <Route path="taskfailure" element={<TaskFailure />} />
-        </Routes>
-      </Router>
-    </TaskContext.Provider>
+    // <TaskProvider>
+    <Router>
+      <Routes>
+        <Route path="alltasks" element={<AllTasks />} />
+        <Route path="mytasks" element={<MyTaskList />} />
+        <Route path="petselection" element={<AnimalSelection />} />
+        <Route path="gamego" element={<GetTask />} />
+        <Route path="tasktimer" element={<TaskTimer />} />
+        <Route path="tasksuccess" element={<TaskSuccess />} />
+        <Route path="taskfailure" element={<TaskFailure />} />
+      </Routes>
+    </Router>
+    // </TaskProvider>
   );
 }
 
